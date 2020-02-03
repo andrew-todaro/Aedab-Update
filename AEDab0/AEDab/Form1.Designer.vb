ea9998e4360db4eb37b7794fb890716a6552a6a2
Andrew Rucker Todaro <andrew.todaro@outlook.com>
2020-02-02 21:55:54 -0600

    Add files via upload

---
+++ AEDab0/AEDab/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Flash = New AxShockwaveFlashObjects.AxShockwaveFlash()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Layer1 = New System.Windows.Forms.NumericUpDown()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.CustomTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        CType(Me.Flash, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Layer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Flash
+        '
+        Me.Flash.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Flash.Enabled = True
+        Me.Flash.Location = New System.Drawing.Point(3, 31)
+        Me.Flash.Name = "Flash"
+        Me.Flash.OcxState = CType(resources.GetObject("Flash.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.Flash.Size = New System.Drawing.Size(800, 600)
+        Me.Flash.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TextBox1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(85, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(77, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 50
+        '
+        'Button3
+        '
+        Me.Button3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button3.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(168, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Inft H/M/S"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Button4.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(249, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Instant Kill"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(76, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Reload SWF"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(560, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Set Variable"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TextBox2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(387, 5)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(77, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.TextBox3.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(503, 5)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox3.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(332, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 14)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Variable:"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(466, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 14)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Value:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 14
+        Me.ListBox1.Location = New System.Drawing.Point(808, 30)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(155, 214)
+        Me.ListBox1.TabIndex = 16
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(874, 7)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 14)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Layer:"
+        '
+        'Layer1
+        '
+        Me.Layer1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Layer1.Location = New System.Drawing.Point(909, 6)
+        Me.Layer1.Name = "Layer1"
+        Me.Layer1.Size = New System.Drawing.Size(54, 20)
+        Me.Layer1.TabIndex = 19
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(840, 253)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(99, 25)
+        Me.Button5.TabIndex = 20
+        Me.Button5.Text = "Load Variables"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(809, 377)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(154, 255)
+        Me.RichTextBox1.TabIndex = 21
+        Me.RichTextBox1.Text = ""
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(846, 284)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(86, 25)
+        Me.Button6.TabIndex = 22
+        Me.Button6.Text = "Set Variables"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'CustomTimer
+        '
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(837, 315)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(108, 18)
+        Me.CheckBox1.TabIndex = 23
+        Me.CheckBox1.Text = "Update Variables"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NumericUpDown1.Location = New System.Drawing.Point(872, 339)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(54, 20)
+        Me.NumericUpDown1.TabIndex = 24
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(837, 341)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 14)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "Rate:"
+        '
+        'Button7
+        '
+        Me.Button7.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Button7.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(736, 3)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(133, 23)
+        Me.Button7.TabIndex = 26
+        Me.Button7.Text = "Stop Variables/SWFs"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(965, 632)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Layer1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Flash)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(981, 671)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(981, 671)
+        Me.Name = "Form1"
+        Me.Text = "AdventureQuest Trainer"
+        CType(Me.Flash, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Layer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Flash As AxShockwaveFlashObjects.AxShockwaveFlash
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Layer1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents CustomTimer As System.Windows.Forms.Timer
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+End Class
---
+++ AEDab0/AEDab/Form1.vb
@@ -0,0 +1,147 @@
+﻿Public Class Form1
+    Sub getswf()
+        Dim sourcecode As String = String.Empty
+        Try
+            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://guardian.battleon.com/game/flash/game.asp?launchtype=large")
+            Dim response As System.Net.HttpWebResponse = request.GetResponse()
+            Dim sr As System.IO.StreamReader = New System.IO.StreamReader(response.GetResponseStream())
+            sourcecode = sr.ReadToEnd
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Try
+            Dim Fst As Integer = sourcecode.IndexOf("src=")
+            Dim Snd As String = sourcecode.Substring(Fst, sourcecode.Length - Fst) : sourcecode = Snd
+            Dim Thrd As Integer = sourcecode.IndexOf("FlashVars=")
+            Dim Frth As String = sourcecode.Substring(0, Thrd)
+            Dim fth As String = "src="
+            Dim sth() As String = {fth}
+            Dim svn = Frth.Split(sth, 2, StringSplitOptions.RemoveEmptyEntries)
+            Dim ate As String = svn(1)
+            Dim Final As String = ate.Replace("""", "")
+            Dim f2() As String = {fth}
+            Dim f3 = Final.Split(sth, 2, StringSplitOptions.RemoveEmptyEntries)
+            TextBox1.Text = f3(1)
+        Catch ex As Exception
+            MsgBox("No web access / AE website has broke autofetch code, manually find and enter the new SWF by going to the games website and right clicking then view page source and search for .swf")
+        End Try
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        getswf()
+        Flash.Base = "http://guardian.battleon.com/game/flash/game.asp?launchtype=large"
+        Flash.Movie = "http://guardian.battleon.com/game/flash/Lore451.swf?ver=2" & TextBox1.Text
+        Flash.Play()
+        Try
+            Dim dir As New IO.DirectoryInfo(Application.StartupPath & "/data/")
+            Dim files As IO.FileInfo() = dir.GetFiles("*.swf")
+            Dim fileName As IO.FileInfo
+
+            For Each fileName In files
+                ListBox1.Items.Add(fileName.Name)
+            Next
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick, Timer.Tick
+        Try
+            Dim value As Integer = Flash.GetVariable("_root.player.intBaseSP")
+            Dim value2 As Integer = Flash.GetVariable("_root.player.intBaseMP")
+            If value + value2 > 0.0 Then
+                Flash.SetVariable("_root.player.intSP", value2)
+                Flash.SetVariable("_root.player.intMP", value)
+            Else
+                Flash.SetVariable("_root.player.intSP", 10000)
+                Flash.SetVariable("_root.player.intMP", 10000)
+            End If
+            Dim num As Integer = Flash.GetVariable("_root.player.intBaseHP")
+            If num > 0 Then
+                Flash.SetVariable("_root.player.intHP", num)
+            Else
+                Flash.SetVariable("_root.player.intHP", num)
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Timer.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Flash.SetVariable("_root.monster.intHP", 0)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Flash.Movie = "http://guardian.battleon.com/Build30/" & TextBox1.Text
+        Flash.GotoFrame(1)
+        Flash.Play()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Flash.SetVariable(TextBox2.Text, textbox3.text)
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Flash.SetVariable("_root.player.intAccessLevel", 22) 'Not working
+        Flash.SetVariable("_root.elite", 13)
+        Flash.LoadMovie(Layer1.Value, Application.StartupPath & "/Data/" & ListBox1.SelectedItem)
+        Flash.SetVariable("_root.elite", 13)
+        Layer1.Minimum = Layer1.Value
+        Layer1.Value += 1
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then RichTextBox1.LoadFile(OpenFileDialog1.FileName, RichTextBoxStreamType.PlainText)
+    End Sub
+    Dim arr As Array
+    Dim lines As Array
+    Sub dum(ByRef lines As RichTextBox)
+        If lines.Text.Length > 0 Then
+            arr = {}
+            For Each c As String In lines.Lines
+                Try
+                arr = c.Split(" ")
+                    Flash.SetVariable(arr(0), arr(1))
+                Catch ex As Exception
+                End Try
+            Next
+        End If
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If CheckBox1.Checked Then
+            If CustomTimer.Enabled = True Then
+                CustomTimer.Enabled = False
+            Else : End If
+            CustomTimer.Interval = NumericUpDown1.Value
+            CustomTimer.Enabled = True
+        Else
+
+            dum(RichTextBox1)
+
+        End If
+    End Sub
+
+    Private Sub CustomTimer_Tick(sender As Object, e As EventArgs) Handles CustomTimer.Tick
+        dum(RichTextBox1)
+    End Sub
+    Dim X As Integer = 1
+    Sub fuck()
+        Do Until X > Layer1.Value
+            Try
+                Flash.LoadMovie(X, "null.swf") : X += 1
+            Catch ex As Exception
+
+            End Try
+        Loop
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Timer.Enabled = False
+        CustomTimer.Enabled = False
+        Layer1.Minimum = 1
+        fuck()
+        X = 1
+    End Sub
+
+End Class
